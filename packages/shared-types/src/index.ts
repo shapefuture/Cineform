afff5a345a1551c4e29a560deb3b5d899a3c0635
team.agcy <team.agcy@gmail.com>
2025-05-04 07:52:15 +0000

    feat: add interfaces for animation and project data
    Co-authored-by: Genie <genie@cosine.sh>

--- packages/shared-types/src/index.ts
+++ packages/shared-types/src/index.ts
@@ -1,1 +1,10 @@
-// Entry point for @cineform-forge/shared-types
+export * from './interfaces/ProjectData';
+export * from './interfaces/ProjectMetadata';
+export * from './interfaces/AnimationElement';
+export * from './interfaces/Keyframe';
+export * from './interfaces/AnimationSequence';
+export * from './interfaces/TimelineData';
+export * from './interfaces/RenderingOptions';
+export * from './interfaces/ScrollTrigger';
+export * from './interfaces/AnimationSuggestion';
+// Add exports for any constants defined in constants/ later
---
+++ packages/shared-types/src/interfaces/TimelineData.ts
@@ -0,0 +1,1 @@
+// Placeholder for TimelineData interface.
